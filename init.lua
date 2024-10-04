fe13afd7548cde40dad67edc3aa0f994fa18c007
Tobias Rettenmeier <tobias.rettenmeier@markt-pilot.com>
2024-10-04 10:20:39 +0200

    added percent in status line

--- init.lua
+++ init.lua
@@ -801,7 +801,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l:%-2v [%p%%]'
       end
 
       -- ... and there is more!
